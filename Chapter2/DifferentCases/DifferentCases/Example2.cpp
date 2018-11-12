60c04a8a49042f240b277413af696b291c51d229
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2018-11-11 22:10:06 -0600

    Threading safe

--- Chapter2/DifferentCases/DifferentCases/Example2.cpp
+++ Chapter2/DifferentCases/DifferentCases/Example2.cpp
@@ -1,27 +1,45 @@
-#include <iostream>
-#include<thread>
+#define _CRT_SECURE_NO_WARNINGS
 
-struct func
-{
+#include <iostream>
+#include <thread>
+
+char *  getCurrentTime() {
+	time_t _tm = time(NULL);
+	struct tm * curtime = localtime(&_tm);
+	return asctime(curtime);
+}
+
+struct func {
 	int & i;
 
 	func(int& i_) :i(i_) {}
 
-	void operator()()
-	{
-		for (unsigned j = 0; j < 1000000; ++j)
-		{
-			i += j;
-		}
-		std::cout << "Finished" << i << std::endl;
+	void operator()() {
+
+		time_t start, end;
+
+		double diff;
+
+		time(&start); 
+		
+		std::cout << "Started at " << getCurrentTime() << '\n';
+
+		for (unsigned j = 0; j < 100000000; ++j) { i += j; }
+
+		time(&end);
+
+		diff = difftime(end, start);
+
+		std::cout << "Finished, total value : " << i << '\n' <<"Finished at: " << getCurrentTime() << '\n';
+
+		std::cout << "Total duration: " << diff << " seconds" << '\n';
 	}
 };
 
-void oops()
-{
+void oops() {
 	int some_local_state = 0;
 	func my_func(some_local_state);
-	std::cout << my_func.i << std::endl;
+
 	std::thread my_thread(my_func);
 	std::cout << my_func.i << std::endl;
 	//Case 1: Does not wait for the thread to end, value of i changes after oops finishes
@@ -35,8 +53,61 @@ void oops()
 	std::cout << my_func.i << std::endl;
 }
 
+/*
+	This function is to visualize that a thread could throw an error while is executing
+	that is why is good to use try/cath before the thread finishes
+*/
+
+void f() {
+	int some_local_state = 0;
+	func my_func(some_local_state);
+	std::thread t(my_func);
+	try {
+		std::cout<<"Doing something while thread 't' runs " << std::endl;
+	}
+	catch (...) {
+		t.join();
+		throw;
+	}
+
+	t.join();
+
+	std::cout << my_func.i << std::endl;
+}
+
+/*
+	Another way to make safer threads is to override the destructor function
+*/
+
+class thread_guard {
+	std::thread& t;
+public:
+
+	explicit thread_guard(std::thread& t_) : t(t_){}
+	
+	~thread_guard(){
+		if (t.joinable()) { //This is important because join can only be called once
+			t.join();
+			std::cout << "Thread deleted with destructor function " << std::endl;
+		}
+	}
+
+	thread_guard(thread_guard const&) = delete; //Copy constructor deletes to execute the destuctor function of thread_guard
+	thread_guard& operator=(thread_guard const&) = delete;
+};
+
+void thread_guarding() {
+	int some_local_state = 0;
+	func my_func(some_local_state);
+	std::thread t(my_func);
+	thread_guard g(t);
+	std::cout << "Doing something while thread 't' runs " << std::endl;
+}
+
 int main() {
-	oops();
+	//oops();
+	f();
+	//thread_guarding();
 	std::cin.get();
 	return 0;
 }
